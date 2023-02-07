a142d68f5da9d6b483a5cc0e86d940841cee5fd2
Lau Balleby Hansen <lau.balleby.hansen@beumer.com>
2023-02-07 15:01:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomSignalToSignalTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
